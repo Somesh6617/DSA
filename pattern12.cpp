c64b72f397cf9c037f1062e8e0362a62778bc4f2
Somesh6617 <someshwar6617@gmail.com>
2025-04-21 21:48:33 +0530

    Add files via upload

---
+++ pattern12.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"Enter no.of rows: ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            cout<<" ";//for spaces
+        }
+         for(j=0;j<2*i+1;j++)
+        {
+             cout<<"*";   //for *
+        }
+        for(j=0;j<n-i-1;j++)
+        {
+            cout<<" ";//for spaces
+        }
+        cout<<endl;
+    }
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            cout<<" ";//for spaces
+        }
+         for(j=0;j<2*n-2*i-1;j++)
+        {
+             cout<<"*";   //for *
+        }
+        for(j=0;j<i;j++)
+        {
+            cout<<" ";//for spaces
+        }
+        cout<<endl;
+    }
+    return 0;
+}
