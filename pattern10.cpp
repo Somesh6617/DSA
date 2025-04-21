a76ef91428e9170953b5eabe6d731ee8c80b2616
Somesh6617 <someshwar6617@gmail.com>
2025-04-21 21:47:46 +0530

    Add files via upload

--- pattern10.cpp
+++ pattern10.cpp
@@ -23,3 +23,25 @@ int main()
     }
     return 0;
 }
+                //or
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"Enter no.of rows: ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            cout<<" ";//for spaces
+        }
+         for(j=0;j<2*i+1;j++)
+        {
+             cout<<"*";   //for *
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
